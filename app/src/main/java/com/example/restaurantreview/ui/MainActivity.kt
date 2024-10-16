5a2b7696cef9c59947960d905f02393a676d3a7e
SaukiFutaki <utaki.sauki19@gmail.com>
2024-10-16 23:05:38 +0700

    refactor MainActivity && add MainViewModel

--- app/src/main/java/com/example/restaurantreview/ui/MainActivity.kt
+++ app/src/main/java/com/example/restaurantreview/ui/MainActivity.kt
@@ -41,7 +41,6 @@ class MainActivity : AppCompatActivity() {
         val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
         binding.rvReview.addItemDecoration(itemDecoration)
 
-        findRestaurant()
 
         binding.btnSend.setOnClickListener{
             view -> postReview(binding.edReview.text.toString())
@@ -73,31 +72,7 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun findRestaurant() {
-        showLoading(true)
-        val client = ApiConfig.getApiService().getRestaurant(RESTAURANT_ID)
-        client.enqueue(object : Callback<RestaurantResponse> {
-            override fun onResponse(
-                call: Call<RestaurantResponse>,
-                response: Response<RestaurantResponse>
-            ) {
-                showLoading(false)
-                if (response.isSuccessful) {
-                    val responseBody = response.body()
-                    if (responseBody != null) {
-                        setRestaurantData(responseBody.restaurant)
-                        setReviewData(responseBody.restaurant.customerReviews)
-                    }
-                } else {
-                    Log.e(TAG, "onFailure: ${response.message()}")
-                }
-            }
-            override fun onFailure(call: Call<RestaurantResponse>, t: Throwable) {
-                showLoading(false)
-                Log.e(TAG, "onFailure: ${t.message}")
-            }
-        })
-    }
+
     private fun setRestaurantData(restaurant: Restaurant) {
         binding.tvTitle.text = restaurant.name
         binding.tvDescription.text = restaurant.description
